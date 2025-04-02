20ebf640e2924d1c5d1cdfddaf1501553676fda6
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-02 13:00:31 +0200

    -

---
+++ ModeloFutbolista/Futbolista.js
@@ -0,0 +1,8 @@
+class Futbolista {
+    constructor(nombre, apellido, año, posicion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.año = año;
+        this.posicion = posicion;
+    }
+}
