0b445938682ede0d2ffd65e0e6de6e4162aefdf3
Diego-Fernandez-Dominguez <diego.fernandez@iesnervion.es>
2025-04-09 13:14:11 +0200

    Añadido de booleanos en metodos de los modelos

--- js/Modelo/ModeloFutbolista.js
+++ js/Modelo/ModeloFutbolista.js
@@ -28,31 +28,39 @@ class ModeloFutbolista {
     // Funcion para eliminar un futbolista del modelo por nombre y apellido
     eliminarFutbolista(nombre, apellido) {
 
-        id=this.buscarFutbolista(nombre, apellido);
+        const id = this.buscarFutbolista(nombre, apellido);
 
-        if (id != undefined) {
+        let sePudo = false;
 
-        for (let i = 0; i < this.futbolistas.length; i++) {
-            if (this.futbolistas[i].id === id) {
-                this.futbolistas.splice(i, 1);
-                break;
+        if (id !== undefined) {
+            let i = 0;
+            while (!sePudo && i < this.futbolistas.length) {
+                if (this.futbolistas[i].id === id) {
+                    this.futbolistas.splice(i, 1);
+                    sePudo = true;
+                }
+                i++;
             }
+            localStorage.setItem("futbolistas", JSON.stringify(this.futbolistas));
         }
-        localStorage.setItem("futbolistas", JSON.stringify(this.futbolistas));
+        return sePudo;
     }
-}
 
     // Funcion para buscar un futbolista por nombre y apellido
     buscarFutbolista(nombre, apellido) {
-        for (let i = 0; i < this.futbolistas.length; i++) {
+
+        let i = 0;
+
+        let sePudo = false;
+
+        while (!sePudo === false && i < this.futbolistas.length) {
             if (this.futbolistas[i].nombre === nombre && 
                 this.futbolistas[i].apellido === apellido) {
+                sePudo = true;
                 return this.futbolistas[i].id;
             }
+            i++;
         }
-    }
-
-    contarFutbolistas() {
-        return this.futbolistas.length;
+        return undefined;
     }
 }
